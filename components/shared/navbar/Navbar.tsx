d91602c7ba5d0c058f108a2ffff9067966767c5a
kwamecody <kwamecody@gmail.com>
2025-03-10 19:24:24 +0000

    feat: implemented global search bar

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { UserButton, SignedIn } from "@clerk/nextjs";
 import Theme from "./Theme";
 import MobileNav from "./MobileNav";
+import GlobalSearch from "../search/GlobalSearch";
 const Navbar = () => {
   return (
     <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
@@ -19,7 +20,7 @@ const Navbar = () => {
           <span className="text-primary500">Flow</span>
         </p>
       </Link>
-      {/* GlobalSearch */}
+      <GlobalSearch/>
       <div className="flex-between gap-5">
         <Theme/>
         <SignedIn>
