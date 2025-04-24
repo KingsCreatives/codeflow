c7eadd9cf52dc81a5c9e0c8f01c9db67acf99734
kwame cody <kwamecody@gmail.com>
2025-04-24 15:42:50 +0000

    fix: color conflict with HomeFilter component on meduim devices, added hover effects

--- components/Home/HomeFilters.tsx
+++ components/Home/HomeFilters.tsx
@@ -4,17 +4,17 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 
 const HomeFilters = () => {
-  const active = "recommended";
+  const active = "frequent";
   return (
     <div className="mt-10 flex-wrap gap-3 hidden md:flex">
       {HomePageFilters.map((item) => (
         <Button
           key={item.value}
           onClick={() => {}}
-          className={`body-medium rounded-lg px-6 py-4 capitalize shadow-none ${
+          className={`text-dark400_light700 background-light800_dark300 body-medium rounded-lg px-6 py-4 capitalize shadow-none transition-transform duration-150 hover:scale-105 cursor-pointer ${
             active === item.value
-              ? "bg-primary-100 text-primary-500"
-              : "bg-light-800 text-light-500"
+              ? "!bg-orange-400 !text-zinc-50 dark:!bg-orange-500"
+              : "!text-light-500"
           }`}
         >
           {item.name}
