7ac8489b1f8bb45947bc064d52ce246d99e42876
kwame cody <kwamecody@gmail.com>
2025-06-05 11:37:56 +0000

    chore: updated question by using id of user, used user image on question , updated next.config to get user image from clerk

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -4,21 +4,19 @@ import Question from "@/components/forms/Question";
 import { getUserById } from "@/lib/actions/user.action";
 
 export default async function Page() {
-  // const { userId } = await auth();
-  const userId = "user_clerk_123";
-
+  const { userId } = await auth();
 
   if (!userId) return redirect("/sign-in");
 
   const user = await getUserById({ userId });
 
-  console.log(user);
+
 
   return (
     <div>
       <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
       <div className="mt-9">
-        <Question userId={userId} />
+        <Question userId={user?.id!} />
       </div>
     </div>
   );
