61335b6ac099db9e5c2c6c02cd57e5b1c09ab80f
kwame cody <kwamecody@gmail.com>
2026-01-15 13:19:21 +0000

    feat:implement search functionality for community page

--- components/shared/search/GlobalSearch.tsx
+++ components/shared/search/GlobalSearch.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 const GlobalSearch = () => {
   return (
     <div className='relative w-full max-w-[600px] max-lg:hidden'>
-     <div className="background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-1 rounded-xl px-4">
+     <div className="background-light800_darkgradient relative flex min-h-14 grow items-center gap-1 rounded-xl px-4">
         <Image src="/assets/icons/search.svg" alt="search" width={24} height={24} className='cursor-pointer'/>
         <Input type='text' placeholder='search'
         className='paragraph-regular no-focus placeholder text-dark400_light700 border-none shadow-none outline-none'
